412854f513511faaceac07420a0ca30001e542ed
Albert Fotso <legrandpone1@gmail.com>
2025-02-22 22:08:06 +0100

    Test l'en tete req.headers.origin

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.header("Access-Control-Allow-Credentials", "true");
+    console.log(req.headers.origin)
     next();
 });
   
