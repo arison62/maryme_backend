dbe9f5557289489121b0ae827968628e98fb8303
Albert Fotso <legrandpone1@gmail.com>
2025-02-22 21:54:31 +0100

    Modification des parametre cors

--- src/app.js
+++ src/app.js
@@ -4,7 +4,14 @@ const userRouter = require("./routes/utilisateur.route");
 
 
 const app = express()
-app.use(cors())
+app.use(cors
+(
+    {
+    credentials: true,
+    origin: "*",
+    allowedHeaders: "Content-Type, Authorization"
+}
+))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
