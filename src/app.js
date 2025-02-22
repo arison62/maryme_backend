390e6d486a8d0ed0141b60dcc4c3015f30bd4db1
Albert Fotso <legrandpone1@gmail.com>
2025-02-22 23:09:09 +0100

    Configuration des cors

--- src/app.js
+++ src/app.js
@@ -2,11 +2,31 @@ const express = require("express");
 const cors = require("cors");
 const userRouter = require("./routes/utilisateur.route");
 
-const app = express()
-app.use(cors())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+const app = express();
 
-app.use("/user/auth", userRouter)
+// Configuration détaillée des options CORS
+const corsOptions = {
+  origin: ['http://localhost:5173', 'https://maryme.onrender.com'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  exposedHeaders: ['Content-Range', 'X-Content-Range'],
+  maxAge: 86400 // Cache préflight pendant 24 heures
+};
 
-module.exports = app
+// Appliquer la configuration CORS
+app.use(cors(corsOptions));
+
+// Middleware pour les en-têtes personnalisés
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/user/auth", userRouter);
+
+module.exports = app;
