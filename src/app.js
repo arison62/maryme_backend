245aba2f8ea4d86469551874f76f49f3776a58df
Albert Fotso <legrandpone1@gmail.com>
2025-02-22 22:57:48 +0100

    Suppression code cors

--- src/app.js
+++ src/app.js
@@ -3,32 +3,7 @@ const cors = require("cors");
 const userRouter = require("./routes/utilisateur.route");
 
 
-const allowedOrigins = [
-    "http://localhost:3000",  // Dev
-    "https://maryme.onrender.com" // Prod
-];
-
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, origin); // ✅ Renvoie l'origine au lieu de `true`
-        } else {
-            callback(new Error("CORS non autorisé"));
-        }
-    },
-    credentials: true, // ✅ Important pour les cookies et JWT
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"], // ✅ Vérifie bien cette casse
-};
-
-
-
-
-const app = express()
-app.use(cors(corsOptions));
-
-
-
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
