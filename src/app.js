fecf4659026a848508369786ce8c99af04a34882
Albert Fotso <legrandpone1@gmail.com>
2025-02-22 22:59:27 +0100

    Suppression code cors

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const userRouter = require("./routes/utilisateur.route");
 
-
+const app = express()
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
