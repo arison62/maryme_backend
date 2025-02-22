eee17c027cc0dfa40a3326e10730871fe23e61d5
Albert Fotso <legrandpone1@gmail.com>
2025-02-22 22:04:38 +0100

    Ajout des parametres cors en dur

--- src/app.js
+++ src/app.js
@@ -4,14 +4,14 @@ const userRouter = require("./routes/utilisateur.route");
 
 
 const app = express()
-app.use(cors
-(
-    {
-    credentials: true,
-    origin: "*",
-    allowedHeaders: "Content-Type, Authorization"
-}
-))
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+  
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
